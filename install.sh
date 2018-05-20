15faab6bbde98e4701cd2c3bbec2e87caae0a986
gilmouta <gilmouta@gmail.com>
2018-05-20 19:39:08 +0100

    Use Maven Repository's AWS SDK

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 #CHANGE ME:
 JDK7_PATH="/c/Program Files/Java/jdk1.7.0_80"
-AWS_SDK_PATH="/c/Users/gilmo/Documents/Aulas/4A2S/CNV/aws-java-sdk-1.11.315"
 
 echo "# Setting java home to "$JDK7_PATH
 export PATH=$JDK7_PATH/bin:${PATH}
@@ -12,21 +11,15 @@ export MAVEN_OPTS="-XX:-UseSplitVerifier"
 echo "# Done"
 
 echo "# Compiling project"
-mvn compile -Daws.sdk.path=$AWS_SDK_PATH
+mvn compile
 echo "# Done"
 
 echo "# Instrumenting project"
 cd BIT
-mvn exec:java -Daws.sdk.path=$AWS_SDK_PATH
+mvn exec:java
 cd ..
 echo "# Done"
 
 echo "# Installing project"
-mvn install -Daws.sdk.path=$AWS_SDK_PATH
-echo "# Done"
-
-echo "# Running Webserver"
-cd WebServer
-mvn exec:java -Daws.sdk.path=$AWS_SDK_PATH
-cd ..
+mvn install
 echo "# Done"
